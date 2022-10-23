7add8ba578ddcbfba82f5b950b4e28d8e2f06e1c
KirillVM <forjustice55@gmail.com>
2022-10-23 20:20:18 +0300

    Add checkbox for order and modal window with achivements

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,6 +1,6 @@
 (function(window){
     'use strict';
-    var App=window.App || {};
+    var App = window.App || {};
     var $=window.jQuery;
 
     function FormHandler(selector){
@@ -20,18 +20,67 @@
 
         this.$formElement.on('submit',function (event) {
             event.preventDefault();
-    
+            
+            var agreeButton = document.querySelector('#agreeButton');
+            var refuseButton = document.querySelector('#refuseButton');
             var data = {};
             $(this).serializeArray().forEach(function (item) {
                 data[item.name] = item.value;
                 console.log(item.name + ' is ' + item.value);
             });
+
+            //Прогнать с помощью foreach все куки и проверить есть ли значения achivement или noAchivment
+            //если есть куки не менять и добавить html для отображения доп функцианала (други виды порций кофе)
+            agreeButton.onclick = function () {
+                $('#modal-example').modal('hide');
+                document.cookie = data['emailAddress'] + '=achivement ; expires=7';
+                console.log(document.cookie);
+            }
+
+            refuseButton.onclick = function () {
+                $('#modal-example').modal('hide');
+                document.cookie = data['emailAddress'] + '=noAchivement ; expires=7';
+                console.log('omg no');
+            }
             
+            //this.elements[0].focus();
+
+            if (data['size']==='coffee-zilla' &&
+                data['flavor']!='None' &&
+                data['strength']===document.querySelector('#strengthLevel').max) {
+
+                $('#modal-example').modal('show');
+                //$('#modalButton').attr('data-target', '#modal-example'); (добавление атрибутов к кнопке)
+                //$('#modalButton').attr('data-toggle', 'modal');  (добавление атрибутов к кнопке)
+            }
+            else {
+                this.reset();
+                this.elements[0].focus();
+            }
+
             console.log(data);
             fn(data);
         });
     };
 
+    /*FormHandler.prototype.addStrengthLevelHandler = function () {
+        console.log('Setting coffee strength level handler for');
+
+        this.$outputElement.on('range', function(event){
+            event.preventDefault();
+            
+
+        });
+    };*/
+
     App.FormHandler = FormHandler;
     window.App = App;
 })(window);
+
+$(document).ready(function() {
+    // при нажатии на кнопку #show-modal
+    $('#show-modal').click(function() {
+        // открыть модальное окно #modal-example
+        $("#modal-example").modal('show');
+    });
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,65 @@
 (function(window){
     'use strict';
-    var FORM_SELECTOR = '[data-coffee-order:"form"]';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
 
     var App = window.App || {};
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var CheckList = App.CheckList;
 
     var myTruck = new Truck ('ncc-1701', new DataStore());
     window.myTruck = myTruck;
 
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
     var formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    window.formHandler = formHandler;
+    //formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck)); без check box
+    formHandler.addSubmitHandler(function (data) { 
+        myTruck.createOrder.call(myTruck,data);
+        checkList.addRow.call(checkList,data);
+     });
     console.log(formHandler);
+    
 
 })(window);
+
+var outputUpdate = function (value){
+    'use strict'
+
+    var output = document.querySelector('#strengthValue');
+    output.value = value;
+
+    //var input =  document.querySelector('#strengthLevel');
+    //input.webkitSliderThumb.WebkitAppearance = 'none';
+    //input.backgroundColor = 'red';  
+}
+
+
+
+
+/*var outputUpdate = function (value){
+    var output = document.querySelector('#strengthValue');
+
+    var body = document.querySelector('#bodyPosition');
+    var bodyStyles = getComputedStyle(body);
+
+    var panelBody = document.querySelector('#panelBodyPosition');
+    var panelBodyStyles = getComputedStyle(panelBody);
+
+    var inputRange = document.querySelector('#strengthLevel');
+    var inputRangeStyles = getComputedStyle(inputRange);
+    
+    var rangeStartPosition = (parseFloat(bodyStyles.marginLeft) + parseFloat(bodyStyles.paddingLeft))
+                            + (parseFloat(panelBodyStyles.marginLeft) + parseFloat(panelBodyStyles.paddingLeft))+3.0;
+    var outputStep = parseFloat(value)*parseFloat(inputRangeStyles.width)/100.0;
+    output.value = value;
+    output.style.position = 'absolute';
+    output.style.marginTop = '7px';
+    output.style.left = rangeStartPosition + outputStep + 'px';
+}
+
+window.addEventListener("range", function() {
+    console.log(document.getElementById('#strengthLevel').getBoundingClientRect());
+  })*/
